1ffd196c2616acb0099e357ad9a9608eef6e0718
sarganar <sarganar@gmail.com>
2021-07-27 15:32:46 -0300

    Add emotion/styled-components libs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import TestSC from './pages/TestSC';
 import AppRouter from './routers/AppRouter';
 
 function App() {
   return (
     <div className="App">
+      <TestSC />
       <AppRouter />
     </div>
   );

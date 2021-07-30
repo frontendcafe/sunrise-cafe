980f3137ffeb01d1683a01bf49d21fc44a3aced2
sarganar <sarganar@gmail.com>
2021-07-30 10:12:37 -0300

    Add normalize CSS resets using GlobalStyle feature (from styled-components)

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 import TestSC from './molecules/TestSC';
 import AppRouter from './routers/AppRouter';
+import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   return (
-    <div className="App">
-      <TestSC />
-      <AppRouter />
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <TestSC />
+        <AppRouter />
+      </div>
+    </>
   );
 }
 

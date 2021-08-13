3cde5d2d2911fc025871006bd5c9632d5befb8e7
Victor Olivera <ovictoriox@gmail.com>
2021-08-13 18:23:58 -0300

    Added atoms info, order

---
+++ src/atoms/OrderTotal.js
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+import colors from "../styles/colors";
+
+const OrderTotals = styled.p`
+  color: ${colors.blue};
+  font-weight: bold;
+  font-size: 2.5rem;
+  font-weight: 500;
+  width: 366px;
+`;
+
+export default OrderTotals;

a8458469d642af88c057500215e2ec62002793d8
Freud <freud.munera@pragma.com.co>
2022-01-17 10:01:27 -0500

    Atom coffee banner

---
+++ src/atoms/CoffeeBanner.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+import H1 from '../atoms/H1';
+import IconCoffee from '../IconCoffee.svg';
+
+const Logo = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  gap: 1rem;
+`;
+
+const LogoIcon = styled.img`
+  width: 75px;
+`;
+
+LogoIcon.defaultProps = {
+  src: IconCoffee,
+};
+
+export const CoffeeBanner = () => {
+  return (
+    <Logo>
+      <LogoIcon />
+      <H1>FrontEnd Sunrise Café</H1>
+    </Logo>
+  );
+};
--- src/molecules/Header.jsx
+++ src/molecules/Header.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import H1 from "../atoms/H1";
-import styled from "@emotion/styled";
-import H3 from "../atoms/H3";
-import IconCoffe from "../IconCoffe.svg";
+import React from 'react';
+import styled from '@emotion/styled';
+
+import H3 from '../atoms/H3';
+import {CoffeeBanner} from '../atoms/CoffeeBanner';
 
 const Container = styled.div`
   display: flex;
@@ -10,21 +10,6 @@ const Container = styled.div`
   justify-content: space-between;
   padding: 0 2rem;
 `;
-
-const Logo = styled.div`
-  display: flex;
-  align-items: flex-start;
-  justify-content: space-between;
-  gap: 1rem;
-`;
-
-const LogoIcon = styled.img`
-  width: 75px;
-`;
-
-LogoIcon.defaultProps = {
-  src: IconCoffe,
-};
 
 const Nav = styled.div`
   display: flex;
@@ -36,10 +21,7 @@ const Nav = styled.div`
 export const Header = () => {
   return (
     <Container>
-      <Logo>
-        <LogoIcon />
-        <H1>FrontEnd Sunrise Café</H1>
-      </Logo>
+      <CoffeeBanner />
       <Nav>
         <H3>ACERCA DE</H3>
         <H3>FRONTENDCAFE</H3>

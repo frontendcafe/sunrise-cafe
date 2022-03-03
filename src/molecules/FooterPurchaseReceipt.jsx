e260ea47e41855aa9aa3ba8fb9bb6a89df08c818
Freud Múnera <alexandromunera@gmail.com>
2022-03-03 12:02:01 -0500

    'footer-purchase'

---
+++ src/molecules/FooterPurchaseReceipt.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+import colors from '../styles/colors';
+import Link from '../atoms/Link';
+
+export const FooterPurchaseReceipt = () => {
+  const Container = styled.div`
+    margin: 2rem 0;
+    display: flex;
+  `;
+
+  const Circle = styled.div`
+    background-color: ${colors.dark};
+    border-radius: 50%;
+    margin-right: 2rem;
+    width: 2rem;
+    height: 2rem;
+  `;
+
+  return (
+    <Container>
+      <Circle />
+      <Link>Gracias Por Su Compra En FrontEnd Sunrise Café</Link>
+    </Container>
+  );
+};
